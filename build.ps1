605fd223db5093324062216d55e0adad33cdc14d
Justin-p <justin-p@users.noreply.github.com>
2019-09-26 13:02:16 +0200

    new toc

--- build.ps1
+++ build.ps1
@@ -6,19 +6,21 @@ param (
 )
 
 Function GenerateToc {
-    . $PSScriptRoot\code\powershell\Convert-FolderContentToMarkdownTableOfContents.ps1
-    $main   = Convert-FolderContentToMarkdownTableOfContents -FiletypeFilter '*.md' -BaseFolder $PSScriptRoot -BaseURL '.' -excludefolder "external"
-    #$python = Convert-FolderContentToMarkdownTableOfContents -FiletypeFilter "*.py" -BaseFolder "$PSScriptRoot\code\python" -Subfolder '\code\python' -BaseURL "."
-    #$php    = Convert-FolderContentToMarkdownTableOfContents -FiletypeFilter "*.php" -BaseFolder "$PSScriptRoot\code\php" -Subfolder '\code\php' -BaseURL "."
-    Write-Output "# Notes-and-Snippets
-
-A place where I store my notes from stuff I learned, cheatsheets, code small snippets, etc.
-
-$main
-" > $PSScriptRoot\README.md
-    #Write-Output "$python" > $PSScriptRoot\code\python\python.md
-    #Write-Output "$php" > $PSScriptRoot\code\php\php.md
-    
+#    . $PSScriptRoot\code\powershell\Convert-FolderContentToMarkdownTableOfContents.ps1
+#    $main   = Convert-FolderContentToMarkdownTableOfContents -FiletypeFilter '*.md' -BaseFolder $PSScriptRoot -BaseURL '.' -excludefolder "external"
+#    $python = Convert-FolderContentToMarkdownTableOfContents -FiletypeFilter "*.py" -BaseFolder "$PSScriptRoot\code\python" -Subfolder '\code\python' -BaseURL "."
+#    $php    = Convert-FolderContentToMarkdownTableOfContents -FiletypeFilter "*.php" -BaseFolder "$PSScriptRoot\code\php" -Subfolder '\code\php' -BaseURL "."
+#    Write-Output "# Notes-and-Snippets
+#
+#A place where I store my notes from stuff I learned, cheatsheets, code small snippets, etc.
+#
+#$main
+#" > $PSScriptRoot\README.md
+#    Write-Output "$python" > $PSScriptRoot\code\python\python.md
+#    Write-Output "$php" > $PSScriptRoot\code\php\php.md
+#    
+    Set-Location $PSScriptRoot
+    book sm
 }
 if ($UpdateToc) {
     try {
